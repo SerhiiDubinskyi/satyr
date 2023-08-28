e733613a15ff7ac2af4b681c53a562dad1809dbe
serhii <kolobroad1@gmail.com>
2023-08-28 23:24:16 +0300

    added notification

---
+++ src/popupComponents/components/notifications.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+interface Notification {
+    title: string;
+    body: string;
+}
+
+interface NotificationMenu {
+    notifications: Array<Notification>;
+
+}
+
+const NotificationMenu = () => {
+    return (
+	    <div></div>
+    )
+}
